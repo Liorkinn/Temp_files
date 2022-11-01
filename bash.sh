443afbf7c80cd35bd6adf5ace63f7efc28334246
Liorkinn <57214464+Liorkinn@users.noreply.github.com>
2022-11-01 23:50:54 +0400

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -16,3 +16,16 @@ echo $2
 *)
 echo "пусто"
 esac
+PATH = /home/liora/Qt/5.15.2/gcc_64/bin:/usr/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/liora/.dotnet/tools
+QTDIR = /home/liora/Qt/5.15.2/gcc_64
+
+
+
+/home/liora/Qt/5.15.2/gcc_64/bin: - тут файлы такие как assistaint, balsam iconvert linguist xmlpattern and etc...
+/usr/bin - qmake как я понял
+/usr/local/sbin: пусто
+/usr/local/bin пусто 
+и тд - это path
+
+/home/liora/Qt/5.15.2/gcc_64 путь к bin там должен быть он а также таам есть папка плагинс - это qtdir
+
